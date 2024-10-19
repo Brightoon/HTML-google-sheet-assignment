d889033d847086c05f9a38d211b8dab6902bffaf
Bright Obinna <obinnabright57@gmail.com>
2024-10-18 17:50:25 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form")
+console.log(form);
+// const submitButton = document.querySelector("submit")
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz4LuWvz_LgzEkd7nZBvTL2Argwd39z4D4eAbqMNq3uNSeuOaib57WIhn_UaskpBfA3/exec'
+form.addEventListener('submit', e => {
+// submitButton.disabled = true
+e.preventDefault()
+let requestBody = new FormData(form)
+console.log(requestBody.values);
+fetch(scriptURL, { method: 'POST', body: requestBody})
+.then(response => {
+alert('Success!', response)
+// submitButton.disabled = false
+})
+.catch(error => {
+alert('Error!', error.message)
+// submitButton.disabled = false
+
+   }
+   )
+})
